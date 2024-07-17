18fffe4639d798aadda4a5eace7eb7cb1b017682
Gavin Morrow <gavinfmorrow@gmail.com>
2024-07-17 17:04:22 -0400

    get stuff working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import ListRow from "./ListRow";
+import Input from "./Input";
+import TodoItem, { loadItems, pruneItems } from "./TodoItem";
+
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { v4 as uuid } from "uuid";
 
 function App() {
+  // clear errors from prev run
+  console.clear();
+
+  const [todoItems, _setTodoItems] = useState(loadItems());
+  const setTodoItems = (items: typeof todoItems) => {
+    _setTodoItems(items);
+    // serialize to localstorage
+    const json = JSON.stringify(items);
+    localStorage.setItem("items", json);
+  };
+  const appendTodoItem = (text: string) =>
+    setTodoItems(pruneItems([...todoItems, new TodoItem(uuid(), text)]));
+
+  const setTodoItem = (newItem: TodoItem) => {
+    setTodoItems(
+      todoItems.map((item) => {
+        if (item.id === newItem.id) {
+          item = newItem;
+        }
+        return item;
+      }),
+    );
+  };
+
+  const deleteTodoItem = (target: TodoItem) =>
+    setTodoItems(todoItems.filter((item) => item.id !== target.id));
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  const theme = darkTheme;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={theme}>
+        <h1>Todo</h1>
+
+        {/* <Input addListRow={appendTodoItem} /> */}
+
+        <ul
+          style={{
+            listStyleType: "none",
+            paddingLeft: "0",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {todoItems.map((todoItem) => (
+            <ListRow
+              todoItem={todoItem}
+              setTodoItem={setTodoItem}
+              deleteTodoItem={deleteTodoItem}
+              key={todoItem.id}
+            />
+          ))}
+
+          <ListRow
+            todoItem={new TodoItem(uuid(), "", false)}
+            setTodoItem={() => {}}
+            addItem={appendTodoItem}
+            deleteTodoItem={() => {}}
+            isNew={true}
+            key="new!"
+          />
+        </ul>
+      </ThemeProvider>
     </div>
   );
 }
